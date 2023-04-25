b2c32deb13328999d842fff7743c9e12ceaf8d92
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-04-25 15:29:10 +0300

    initialize react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
